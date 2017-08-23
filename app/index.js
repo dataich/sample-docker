b2d4425d265c07d8315a591174dc5004fb670d4a
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2017-08-23 14:05:38 +0900

    Hello, World!を表示するNode.jsアプリケーションを追加

---
+++ app/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+const port = 3000;
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
